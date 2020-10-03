d013092a4dfc805b81d542eeb2476530558d0881
Colin Brayton <gringolalia@gmail.com>
2020-10-03 14:21:27 -0300

    deploy

---
+++ precache-manifest.96c1f29eb727dfd5aacdf70efd72d9b9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f9a9d2c6680d2c097a",
+    "url": "/css/app.0c35696a.css"
+  },
+  {
+    "revision": "cb05c3f4d86ece5d7f07",
+    "url": "/css/chunk-vendors.daab0ff0.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "ec885583295e5abd38e107ea9ac4eb4e",
+    "url": "/img/nyc-city-hall-station.ec885583.jpg"
+  },
+  {
+    "revision": "9b0794881996006de78b23e7237dd48b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbe0fa0604f479da3756",
+    "url": "/js/about.9413d7f8.js"
+  },
+  {
+    "revision": "02f9a9d2c6680d2c097a",
+    "url": "/js/app.9d9b223c.js"
+  },
+  {
+    "revision": "cb05c3f4d86ece5d7f07",
+    "url": "/js/chunk-vendors.814f46b1.js"
+  },
+  {
+    "revision": "b1a72871dbf9623906e6b2ca2f02c218",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
